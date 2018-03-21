0cfdb7dd1bfc9f4dbb96713a25598220b0212e91
mattstg <mattstger@gmail.com>
2018-03-21 11:53:29 -0400

    My first commit
    Changed the structure so I could showcase git

--- Assets/ExampleClass.cs
+++ Assets/ExampleClass.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class ExampleClass : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
 	
 	// Update is called once per frame
 	void Update () {
         SingletonExample.Instance.TestFuncA();
-        
+        Debug.Log("moo|");   
 	}
 
 }
