b8690b16da38b991373a18a19acff03ceaa085ec
mikebjoyce <mikebjoyce@gmail.com>
2018-04-08 23:43:53 -0400

    Monster Animator | Script Anim Param Setting | State Callback

---
+++ Assets/Resources/AI/BehaviourManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourManager  {
+	//must be all the animation names which have behaviour scripts
+	static string[] stateName = {"Idle","Die","AttackTarget","ChaseTarget","Wander"};
+	private Dictionary<int,string> hashToName = new Dictionary<int, string>();
+	StateMachineBehaviour[] behaviour;
+	Animator anim;
+	Monster monster;
+
+	public BehaviourManager(Animator _anim, Monster _monster){
+		anim = _anim;
+		monster = _monster;
+		behaviour = anim.GetBehaviours<GenericBehaviour> ();
+		foreach(GenericBehaviour gb in behaviour)
+			gb.Initialize (this);
+
+		foreach (string s in stateName)
+			hashToName.Add (Animator.StringToHash (s), s);
+	}
+
+	private string GetName(int nameHash){
+		return hashToName [nameHash];
+	}
+
+	public void BehaviourOutput(Monster.State state, int hash){
+		string name = GetName(hash);
+
+		Debug.Log ("State Name: " + name + ". Current phase: " + state.ToString());
+	}
+}
--- Assets/Scripts/Monsters/MonsterMechanics/MonsterAI.cs
+++ Assets/Scripts/Monsters/MonsterMechanics/MonsterAI.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterAI  {
+	static float timeToWander = 2;
     Monster monster;
+	Animator anim;
+	BehaviourManager behaviourManager;
+	float idleTimer = 0;
+	bool wandering = false;
+	bool agressive = false;
 
-	public void Initialize(Monster _monster)
+	public MonsterAI(Monster _monster)
     {
         monster = _monster;
+		anim = monster.GetComponent<Animator> ();
+		anim.runtimeAnimatorController = RuntimeAnimatorController.Instantiate<RuntimeAnimatorController> (Resources.Load ("Resources/AI/BaseAI") as RuntimeAnimatorController);
+		behaviourManager = new BehaviourManager (anim, monster);
     }
+
+	public void Refresh(float dt, bool seePlayer, bool attackRange){
+		anim.SetBool ("SeePlayer", seePlayer);
+		anim.SetBool ("SeePlayer", attackRange);
+
+		if (!agressive && seePlayer) {
+			agressive = true;
+			idleTimer = 0;
+			wandering = false;
+		} else if (agressive && !seePlayer)
+			agressive = false;
+
+		if (!agressive && !wandering) {
+			idleTimer += dt;
+			if (idleTimer >= timeToWander) {
+				anim.SetTrigger ("Wander");
+				wandering = true;
+			}
+		}
+	}
+
+	public void Dies(){
+		anim.SetTrigger ("Dies");
+	}
 
 }
